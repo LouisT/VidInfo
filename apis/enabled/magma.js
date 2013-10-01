b8c705cadafb5e4cb4a5479a2e9cd038dc7b2d3e
Louis T. <louist@ltdev.im>
2013-09-30 20:16:16 -0700

    Delete magma.js
    Moved to /apis/disabled/

--- apis/enabled/magma.js
+++
@@ -1,9 +0,0 @@
-/*
- VidInfo - Louis T. <LouisT@ltdev.im>
- https://github.com/LouisT/VidInfo
-*/
-module.exports = {
-       url: 'http://mag.ma/info.json?id={:id}',
-       regex: /(?:http:\/\/)?(?:.*\.)?mag\.ma\/(?:.+)\/(\d+)/i,
-       shortcuts: 'mag',
-};
