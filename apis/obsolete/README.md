a6c8d88241ff9c3e08b7f5a961c594f6dde89b6c
Louis T <LouisT@ltdev.im>
2013-12-10 21:10:48 -0800

    Ok, NOW add the obsolete folder.

---
+++ apis/obsolete/README.md
@@ -0,0 +1,7 @@
+*Old or obsolete APIs!*
+-------
+
+The files listed here are __OLD__ / __OBSOLETE__ due to API changes, outages, or other issues out of my control. 
+I have no idea when or if they will ever be back, so you can keep an eye on them if you want. 
+Create an [issue](https://github.com/LouisT/VidInfo/issues "Title") if they're fixed/back online. 
+Just because they're in here does not mean a replacement hasn't been created! See IMDB.
---
+++ apis/obsolete/imdbcom.js
@@ -0,0 +1,13 @@
+/*
+ VidInfo - Louis T. <LouisT@ltdev.im>
+ https://github.com/LouisT/VidInfo
+
+ Not really a direct video link, but video information nevertheless.
+ This uses a 3rd party API. IMDB does not have an API.
+ Please see http://mymovieapi.com/#notes for more information.
+*/
+module.exports = {
+       url: 'http://mymovieapi.com/?id={:id}&type=json&plot=full&episode=1&lang=en-US&aka=simple&release=simple',
+       regex: /http:\/\/(?:.*\.)?imdb\.com\/title\/(tt(\d+))\/?/i,
+       shortcuts: 'imdb',
+};
